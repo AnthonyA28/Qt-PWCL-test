478f36de661632e65248db93efca4cb7fcf7a5ea
Anthony_Arrowood <arrowood.aj@ufl.edu>
2018-12-30 11:36:42 -0500

    added error log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+FILE *pFile= fopen("appOutput.log", "w");
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    fprintf(pFile, "%s", localMsg.constData());
+    fflush(pFile);
+
+}
+
 int main(int argc, char *argv[])
 {
+    qInstallMessageHandler(myMessageOutput); // Install the handler
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
+
+    fclose (pFile);
 }
