069d5d42ae74f4f0c03ced02c9d5125b7a822c94
Anthony Arrowood <arrowood.aj@ufl.edu>
2019-01-14 03:07:32 -0500

    Enabled High DPI scaling and made all widgets prefered and non pixel sized

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main(int argc, char *argv[])
 {
     qInstallMessageHandler(myMessageOutput); // Install the handler
 
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
